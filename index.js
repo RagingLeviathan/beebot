11e67accb3e9b1e36fa0c14bab327a2db6d5a1a1
Nicole Graham <FieldsOfSeconds@hotmail.com>
2022-01-23 21:00:45 +0000

    beebot improvments
    * tidied up code
    * added script to keep bot alive/not sleep
    * added json file for better data ingestion
    * added setinterval to ping specified users at 12pm daily if birthday is found

--- index.js
+++ index.js
@@ -1,143 +1,157 @@
-const Discord = require("discord.js")
-const fetch = require("node-fetch")
-const client = new Discord.Client()
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const Database = require("@replit/database");
+const db = new Database();
+const keepAlive = require("./server");
 
-// sadWords = ["sad", "depressed", "unhappy", "angry", "miserable"]
-// encouragements = [
-//   "Cheer up!",
-//   "Hang in there.",
-//   "You are a great person / bot!"
-// ]
-birthdays = [
-  [1, "Sat Jan 22 2021", "Warwick"],
-  [3, 4],
-  [5, 6]
-];
+//todo make function more reuseable
+/***
+ * function for automatically checking if there are birthdays today
+ */
+function birthdayCheck() {
+  const d = new Date();
+  let text = d.toString();
+  let trimmed = text.substring(4, 10);
 
-function getQuote() {
-  return fetch("https://zenquotes.io/api/random")
-    .then(res => {
-      return res.json()
-      })
-    .then(data => {
-      return data[0]["q"] + " -" + data[0]["a"]
-    })
-}
+  //built in nodejs library
+  const fs = require('fs');
+  let data = '';
 
-function createArray(length) {
-    var arr = new Array(length || 0),
-        i = length;
+  fs.readFile("./test.json", "utf8", (err, jsonString) => {
+    if (err) {
+      console.log("File read failed:", err);
+    } else {
+      data = JSON.parse(jsonString);
+      var todaysLot = [];
 
-    if (arguments.length > 1) {
-        var args = Array.prototype.slice.call(arguments, 1);
-        while(i--) arr[length-1 - i] = createArray.apply(this, args);
+      for (var key in data) {
+        if (data.hasOwnProperty(key)) {
+          let fetchedDate = data[key].month + ' ' + data[key].day;
+
+          if (trimmed === fetchedDate) {
+            client.channels.cache.get(process.env.SERVER_BIRTHDAYS_CID).send('<@'+ process.env.ID_1 +'> <@' + process.env.ID_2 + '> Hello there! It\'s ' + data[key].id + '\'s birthday! 🎂 ');
+
+          }
+
+          if (!(trimmed === fetchedDate)) {
+              console.log('No birthdays found today!!!');
+          }
+        }
+      }
     }
-
-    return arr;
+  });
 }
-
-createArray();     // [] or new Array()
-
-createArray(2);    // new Array(2)
-
-createArray(3, 2); // [new Array(2),
-                   //  new Array(2),
-                   //  new Array(2)]
-
-
 
 client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`)
   // client.user.setAvatar('./bee_bot.jpg')
   // .then(user => console.log(`New avatar set!`))
   // .catch(console.error);
-   client.user.setActivity('The Bee Movie!', { type: 'WATCHING' });
+  client.user.setActivity('The Bee Movie!', { type: 'WATCHING' });
+
+  var interval = setInterval(() => {
+    var hour = new Date().getHours();
+
+    if (hour == 11) {
+      console.log('About to post a birthday!!!');
+      birthdayCheck();
+      clearInterval(interval); //
+    }
+  }, 1000); // check once a minute
+
 })
 
+
 client.on("message", msg => {
-  // if (msg.content === "$inspire") {
-  //   getQuote().then(quote => msg.channel.send(quote))
-  // }
 
   if (msg.content === "$birthday" || msg.content === "🎂") {
     const d = new Date();
     let text = d.toString();
-    let trimmed = text.substring(0, 15);
+    let trimmed = text.substring(4, 10);
 
-    let museName = '';
-    for(var i = 0; i < birthdays.length; i++) {
-        var dob = birthdays[i];
-        for(var j = 0; j < dob.length; j++) {
-          if (trimmed === dob[j]) {
-              museName += dob[2];
+    //built in nodejs library
+    const fs = require('fs');
+    let data = '';
+    let bday = '';
+    const target = "Might Guy";
 
+
+    fs.readFile("./test.json", "utf8", (err, jsonString) => {
+      if (err) {
+        console.log("File read failed:", err);
+      } else {
+        data = JSON.parse(jsonString);
+
+        for (var key in data) {
+          if (data.hasOwnProperty(key)) {
+            let fetchedDate = data[key].month + ' ' + data[key].day;
+            console.log(data[key].month + ' ' + data[key].day);
+
+            if (trimmed === fetchedDate) {
+              let exampleEmbed = new Discord.MessageEmbed().setTitle('It\'s ' + data[key].id + '\'s birthday! 🎂 ');
+              exampleEmbed.setColor('#5dc4ff');
+              msg.channel.send(exampleEmbed);
+            }
+
+            if (!(trimmed === fetchedDate)) {
+              const exampleEmbed = new Discord.MessageEmbed().setTitle('No muses celebrate their birthday today!');
+              exampleEmbed.setColor('#fcfc55');
+              msg.channel.send(exampleEmbed);
+              exit;
+            }
           }
         }
-    }
+      }
+    });
 
-  if (museName === '') {
-      const exampleEmbed = new Discord.MessageEmbed().setTitle('No muses celebrate their birthday today!');
-    exampleEmbed.setColor('#fcfc55');
-    msg.channel.send(exampleEmbed);
-  } else {
-   const exampleEmbed = new Discord.MessageEmbed().setTitle('It\'s ' + museName + '\'s birthday! 🎂 ');
-    exampleEmbed.setColor('#5dc4ff');
-    msg.channel.send(exampleEmbed);
-   }
   }
 
+  //testing sending custom emoji
+  //to find custom emoji code do;
+  // \:customeemojiname:
+  // & copy result
   if (msg.content === "$test") {
     msg.channel.send("<a:capslap:809918103999873089>");
   }
 
-    if (msg.content === "$thing") {
-// const exampleEmbed = new Discord.MessageEmbed()
-// .setTitle("This is a title")
-// .setTitle("http://tryitands.ee")
-// .setDescription("This is a description")
-// .setTimestamp()
-// .setFooter("This is a footer")
-// .setAuthor("This is the author's name")
-// .addField("This is a field", "this is its description")
-// .setImage("https://i.imgur.com/KlT2KXj.png")
-// .setThumbnail("https://i.imgur.com/C2Mrmwo.png");
-// 	exampleEmbed.setColor('#cc22cc');
-const exampleEmbed = new Discord.MessageEmbed()
-.setDescription("**“Vengeance is only a step behind you.”**")
-.setFooter("Warwick")
-.setTimestamp()
-.setThumbnail("https://i.imgur.com/C2Mrmwo.png");
-	exampleEmbed.setColor('#b8a5ff');
-  msg.channel.send(exampleEmbed);
-};
-
-
-   if (msg.content === "$bees") {
-     const embed = new Discord.MessageEmbed().setTitle('NOT THE BEES!!!').setImage('attachment://bees.gif');
-msg.channel.send({ embeds: [embed], files: ['./bees.gif'] });
-   // msg.channel.send("./bees.gif");
-  }
-
-     if (msg.content === "$englishftw") {
-      msg.channel.send("https://www.youtube.com/watch?v=hiwgOWo7mDc");
-      msg.channel.send("🎈🎈🎈");
-  }
-
-
-     if (msg.content === "$fancy") {
-     const exampleEmbed = new Discord.MessageEmbed().setTitle('Here be dragons! 🔥🐲');
-
-	exampleEmbed.setColor('#1bd928');
+  //testing embeds
+  //bones of quote function
+  if (msg.content === "$thing") {
+    // const exampleEmbed = new Discord.MessageEmbed()
+    // .setTitle("This is a title")
+    // .setTitle("http://tryitands.ee")
+    // .setDescription("This is a description")
+    // .setTimestamp()
+    // .setFooter("This is a footer")
+    // .setAuthor("This is the author's name")
+    // .addField("This is a field", "this is its description")
+    // .setImage("https://i.imgur.com/KlT2KXj.png")
+    // .setThumbnail("https://i.imgur.com/C2Mrmwo.png");
+    // 	exampleEmbed.setColor('#cc22cc');
+    const exampleEmbed = new Discord.MessageEmbed()
+      .setDescription("**“Vengeance is only a step behind you.”**")
+      .setFooter("Warwick")
+      .setTimestamp()
+      .setThumbnail("https://i.imgur.com/C2Mrmwo.png");
+    exampleEmbed.setColor('#b8a5ff');
     msg.channel.send(exampleEmbed);
+  };
+
+
+  //testing sending gifs
+  if (msg.content === "$bees") {
+    const embed = new Discord.MessageEmbed().setTitle('NOT THE BEES!!!').setImage('attachment://bees.gif');
+    msg.channel.send({ embeds: [embed], files: ['./bees.gif'] });
+    // msg.channel.send("./bees.gif");
   }
 
-  // if (sadWords.some(word => msg.content.includes(word))) {
-  //   const encouragement = encouragements[Math.floor(Math.random() * encouragements.length)]
-  //   msg.reply(encouragement)
-  // }
+  //testing sending yt links
+  if (msg.content === "$englishftw") {
+    msg.channel.send("https://www.youtube.com/watch?v=hiwgOWo7mDc");
+    msg.channel.send("🎈🎈🎈");
+  }
 
-  
+});
 
-})
-
-client.login(process.env.TOKEN)
+keepAlive(); //keeps server awake
+client.login(process.env.TOKEN);
