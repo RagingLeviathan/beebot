4949408bae36dfd6d0d52deac951f4fe330531b0
Nicole Graham <FieldsOfSeconds@hotmail.com>
2022-01-23 00:41:47 +0000

    beebot
    * improved birthday function
    * made a start on adding quotes function
    * other experiments made

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const client = new Discord.Client()
 //   "You are a great person / bot!"
 // ]
 birthdays = [
-  [1, "Sat Jan 22 2022"],
+  [1, "Sat Jan 22 2021", "Warwick"],
   [3, 4],
   [5, 6]
 ];
@@ -46,8 +46,6 @@ createArray(3, 2); // [new Array(2),
 
 
 
-
-
 client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`)
   // client.user.setAvatar('./bee_bot.jpg')
@@ -63,17 +61,56 @@ client.on("message", msg => {
 
   if (msg.content === "$birthday" || msg.content === "🎂") {
     const d = new Date();
-let text = d.toString();
-let trimmed = text.substring(0, 15)
-let compared = birthdays[0][1];
-if (trimmed === compared)
-   // msg.reply("It's Warwick's birthday!");
-    msg.channel.send("It's Warwick's birthday!");
+    let text = d.toString();
+    let trimmed = text.substring(0, 15);
+
+    let museName = '';
+    for(var i = 0; i < birthdays.length; i++) {
+        var dob = birthdays[i];
+        for(var j = 0; j < dob.length; j++) {
+          if (trimmed === dob[j]) {
+              museName += dob[2];
+
+          }
+        }
+    }
+
+  if (museName === '') {
+      const exampleEmbed = new Discord.MessageEmbed().setTitle('No muses celebrate their birthday today!');
+    exampleEmbed.setColor('#fcfc55');
+    msg.channel.send(exampleEmbed);
+  } else {
+   const exampleEmbed = new Discord.MessageEmbed().setTitle('It\'s ' + museName + '\'s birthday! 🎂 ');
+    exampleEmbed.setColor('#5dc4ff');
+    msg.channel.send(exampleEmbed);
+   }
   }
 
   if (msg.content === "$test") {
     msg.channel.send("<a:capslap:809918103999873089>");
   }
+
+    if (msg.content === "$thing") {
+// const exampleEmbed = new Discord.MessageEmbed()
+// .setTitle("This is a title")
+// .setTitle("http://tryitands.ee")
+// .setDescription("This is a description")
+// .setTimestamp()
+// .setFooter("This is a footer")
+// .setAuthor("This is the author's name")
+// .addField("This is a field", "this is its description")
+// .setImage("https://i.imgur.com/KlT2KXj.png")
+// .setThumbnail("https://i.imgur.com/C2Mrmwo.png");
+// 	exampleEmbed.setColor('#cc22cc');
+const exampleEmbed = new Discord.MessageEmbed()
+.setDescription("**“Vengeance is only a step behind you.”**")
+.setFooter("Warwick")
+.setTimestamp()
+.setThumbnail("https://i.imgur.com/C2Mrmwo.png");
+	exampleEmbed.setColor('#b8a5ff');
+  msg.channel.send(exampleEmbed);
+};
+
 
    if (msg.content === "$bees") {
      const embed = new Discord.MessageEmbed().setTitle('NOT THE BEES!!!').setImage('attachment://bees.gif');
@@ -81,10 +118,25 @@ msg.channel.send({ embeds: [embed], files: ['./bees.gif'] });
    // msg.channel.send("./bees.gif");
   }
 
+     if (msg.content === "$englishftw") {
+      msg.channel.send("https://www.youtube.com/watch?v=hiwgOWo7mDc");
+      msg.channel.send("🎈🎈🎈");
+  }
+
+
+     if (msg.content === "$fancy") {
+     const exampleEmbed = new Discord.MessageEmbed().setTitle('Here be dragons! 🔥🐲');
+
+	exampleEmbed.setColor('#1bd928');
+    msg.channel.send(exampleEmbed);
+  }
+
   // if (sadWords.some(word => msg.content.includes(word))) {
   //   const encouragement = encouragements[Math.floor(Math.random() * encouragements.length)]
   //   msg.reply(encouragement)
   // }
+
+  
 
 })
 
